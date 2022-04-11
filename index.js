2b6997c23c1af9d0a8e2f4dd01a8a17944985685
Ana Zitzow <anasempai89@gmail.com>
2022-04-11 15:25:52 -0700

    commit tree 15fab05a1819375e4b507f8c407a6a57afc67b36

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Follow along with the examples here
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+console.log('Hello, world!');
+
+function helloWorld(greeting) {
+  for (let i = 0; i < 5; i++){
+    console.log(greeting);
+  }
+};
+
+helloWorld('Hello, World!');
+
+function doSomething() {
+  console.log('Hello, world!');
+};
+
+doSomething();
+
+function sayHello(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+sayHello('Ana');
+
+function greetings(greeting, firstName) {
+  return `${greeting}, ${firstName}!`;
+};
+
+console.log(greetings('Nice to meet you', 'Ana'));
